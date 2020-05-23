d0041537c06babe4b525b5879502fcf8e3095114
longpt99 <phuongthanhlong.99@gmail.com>
2020-05-23 19:40:17 +0700

    add some plugin

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,38 +1,39 @@
 /* eslint-disable no-undef */
 document.addEventListener('DOMContentLoaded', () => {
   const calendarEl = document.getElementById('calendar');
-
   const calendar = new FullCalendar.Calendar(calendarEl, {
-    eventClick: function(info) {
-      alert('Event: ' + info.event.title);
-      info.el.style.borderColor = 'pink';
-      debugger
-    },
-    plugins: ['dayGrid', 'list', 'timeGrid'],
+    plugins: ['dayGrid', 'list', 'timeGrid', 'interaction'],
+    height: 'auto',
+    slotDuration: '01:00:00',
+    minTime: '09:00:00',
+    maxTime: '19:00:00',
     defaultView: 'timeGridWeek',
+    selectMirror: true,
+    selectable: true,
+
+    eventClick(info) {
+      console.log(info)
+      alert(`Event: ${info.event.title}`);
+      info.el.style.borderColor = 'pink';
+    },
+
+    dateClick(info) {
+      alert(`Date: ${info.date}`);
+      const event = {
+        title: 'Phuong Thanh Long',
+        start: info.dateStr,
+      };
+      const data = JSON.parse(localStorage.getItem('date-info'));
+      if (data) {
+        const events = [].concat(data);
+        events.push(event);
+        localStorage.setItem('date-info', JSON.stringify(events));
+      } else {
+        localStorage.setItem('date-info', JSON.stringify(event));
+      }
+      calendar.addEvent(event);
+    },
   });
+
   calendar.render();
-
-  document.getElementById('add-event').addEventListener('click', () => {
-    const event = {
-      title: 'Phuong Thanh Long',
-      start: new Date(),
-      end: '2020-05-05',
-    }
-    const data = JSON.parse(localStorage.getItem('date-info'));
-    console.log(data)
-    if (data) {
-      const events = [].concat(data);
-      events.push(event)
-      localStorage.setItem('date-info', JSON.stringify(events));
-    } else {
-      localStorage.setItem('date-info', JSON.stringify(evens));
-    }
-    calendar.addEvent(
-       event,
-    );
-  });
-
-  
-
 });
